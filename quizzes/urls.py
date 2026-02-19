7a667dad80705234a26c49a679134576a3043d9d
maksmaks21 <maksroks777@gmai.com>
2026-02-19 20:32:07 +0200

    schneine

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('quiz/<int:quiz_id>/', views.quiz_detail, name='quiz_detail'),
 ]
--- quizzes/views.py
+++ quizzes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Quiz
 
@@ -9,3 +9,10 @@ def home(request):
         'page_title': 'Home Page',
     }
     return render(request, 'quizzes/home.html', context)
+
+def quiz_detail(request, quiz_id):
+    quiz = get_object_or_404(Quiz, id=quiz_id)
+    context = {
+        'quiz': quiz,
+        'page_title': f'Вікторина: {quiz.title}',
+    }
